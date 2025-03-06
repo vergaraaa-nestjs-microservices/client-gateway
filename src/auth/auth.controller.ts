a7e3e051a8dc8037cfaf0a1bc187ba5239d4f5b3
Edgar Vergara <edgarvm13@hotmail.com>
2025-03-06 16:16:28 +0100

    Implement user registration and login DTOs with validation; enhance AuthController to handle user registration and login requests with error handling

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,23 +1,50 @@
-import { Controller, Get, Inject, Post } from '@nestjs/common';
-import { ClientProxy } from '@nestjs/microservices';
+import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { NATS_SERVICE } from 'src/config/services';
+import { RegisterUserDto } from './dto/register-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
+import { catchError } from 'rxjs';
 
 @Controller('auth')
 export class AuthController {
   constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
 
   @Post('register')
-  register() {
-    return this.client.send('auth.register.user', {});
+  register(@Body() registerUserDto: RegisterUserDto) {
+    return this.client.send('auth.register.user', registerUserDto).pipe(
+      catchError((error) => {
+        if (typeof error === 'object') {
+          throw new RpcException(error as object);
+        }
+
+        throw new RpcException('Unknown error');
+      }),
+    );
   }
 
   @Post('login')
-  login() {
-    return this.client.send('auth.login.user', {});
+  login(@Body() loginUserDto: LoginUserDto) {
+    return this.client.send('auth.login.user', loginUserDto).pipe(
+      catchError((error) => {
+        if (typeof error === 'object') {
+          throw new RpcException(error as object);
+        }
+
+        throw new RpcException('Unknown error');
+      }),
+    );
   }
 
   @Get('verify')
   verify() {
-    return this.client.send('auth.verify.user', {});
+    return this.client.send('auth.verify.user', {}).pipe(
+      catchError((error) => {
+        if (typeof error === 'object') {
+          throw new RpcException(error as object);
+        }
+
+        throw new RpcException('Unknown error');
+      }),
+    );
   }
 }
